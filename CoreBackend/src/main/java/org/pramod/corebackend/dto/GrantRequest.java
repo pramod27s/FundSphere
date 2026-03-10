ce3ed67784e17130cc6106847cb27a3a95bf8f40
pramod27s <mrpramod3@gmail.com>
2026-03-10 22:55:11 +0530

    api creation of grants info

---
+++ CoreBackend/src/main/java/org/pramod/corebackend/dto/GrantRequest.java
@@ -0,0 +1,32 @@
+package org.pramod.corebackend.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GrantRequest {
+
+    private String grantTitle;
+    private String fundingAgency;
+    private String programName;
+    private String description;
+    private String grantUrl;
+    private LocalDateTime applicationDeadline;
+    private BigDecimal fundingAmountMin;
+    private BigDecimal fundingAmountMax;
+    private String fundingCurrency;
+    private String eligibleCountries;
+    private String eligibleApplicants;
+    private String institutionType;
+    private String field;
+    private String applicationLink;
+    private String checksum;
+    private List<String> tags;
+}
---
+++ CoreBackend/src/main/java/org/pramod/corebackend/repository/GrantRepository.java
@@ -0,0 +1,16 @@
+package org.pramod.corebackend.repository;
+
+import org.pramod.corebackend.entity.Grant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GrantRepository extends JpaRepository<Grant, Long> {
+
+    Optional<Grant> findByGrantUrl(String grantUrl);
+
+    boolean existsByGrantUrl(String grantUrl);
+}
+
